50d22133118689c9dbca2666d7c6280c80669aea
Jawairia <Jawairiamazhar21@gmail.com>
2026-07-09 23:28:49 +0500

    Refactor: Clean up ReviewCard component by removing commented-out code and update Reviews page structure

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -3,16 +3,6 @@ import '../css/reviews.css'
 
 const ReviewCard = ({profile, name, rating, review}) => {
   return (
-    // <section className="flex flex-col items-center justify-center p-4 bg-white rounded-lg shadow-md w-80">
-    //     <div className="flex items-center">
-    //         <img src={profile} alt={name} className="w-18 h-18 rounded-full mr-4" />
-    //     </div>
-    //         <div className="flex flex-col items-center text-center border-2 border-[#fcc200] rounded-lg p-4">
-    //             <h3 className="text-lg font-semibold">{name}</h3>
-    //             <p className="text-sm text-gray-600">{rating}</p>
-    //             <p className="text-gray-700 text-center">{review}</p>
-    //         </div>
-    // </section>
     <figure class="snip1533">
   <figcaption>
     <p>{rating}</p>
--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -4,7 +4,7 @@ import reviews from '../data/reviewsData'
 
 const Reviews = () => {
   return (
-    <div>
+    <div id="reviews">
         <h1 className="text-3xl font-bold text-center">What Our Customer Says</h1>
         <div className="mt-8 grid gap-6 md:grid-cols-2 xl:grid-cols-4">
         {reviews.map((review, index) => (
